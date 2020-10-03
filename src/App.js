22164e4b7597f6d2e652c3d1352c26771e84f9fa
renatopcinfo <renatopcinfo@gmail.com>
2020-10-03 16:39:11 -0300

    phone

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import phone from './assets/phone.jpg'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <img src={phone} alt="teste" />   
+  )
 }
 
 export default App;
+
+
+
